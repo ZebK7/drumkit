d11766228398e417623928707cbf2ccc9e9f2c77
ZebK7 <101284819+ZebK7@users.noreply.github.com>
2022-04-26 17:14:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,37 +19,37 @@ document.addEventListener("keypress", function(event) {
   function sound(key) {
     switch (key) {
       case "q":
-        var sound1 = new Audio("music/q.mp3");
+        var sound1 = new Audio("q.mp3");
         sound1.play();
         break;
     
       case "w":
-        var sound2 = new Audio("music/w.mp3");
+        var sound2 = new Audio("w.mp3");
         sound2.play();
         break;
     
       case "e":
-        var sound3 = new Audio('music/e.mp3');
+        var sound3 = new Audio('e.mp3');
         sound3.play();
         break;
     
       case "r":
-        var sound4 = new Audio('music/r.mp3');
+        var sound4 = new Audio('r.mp3');
         sound4.play();
         break;
     
       case "t":
-        var sound5 = new Audio('music/t.mp3');
+        var sound5 = new Audio('t.mp3');
         sound5.play();
         break;
     
       case "y":
-        var sound6 = new Audio('music/y.mp3');
+        var sound6 = new Audio('y.mp3');
         sound6.play();
         break;
     
       case "u":
-        var sound7 = new Audio('music/u.mp3');
+        var sound7 = new Audio('u.mp3');
         sound7.play();
         break;
     
